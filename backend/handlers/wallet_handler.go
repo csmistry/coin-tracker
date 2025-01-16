8d57d213fb09049bfa0f833cd8ccd6ad04d0f1f6
Chintan Mistry <43223499+csmistry@users.noreply.github.com>
2025-01-16 15:14:16 -0500

    build out apis (#3)

---
+++ backend/handlers/wallet_handler.go
@@ -0,0 +1,84 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/csmistry/coin-tracker/backend/pkg/wallet"
+	"github.com/gorilla/mux"
+)
+
+// ListAddresses returns a list of address added to the wallet
+func ListAddresses(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// Call List addresss
+	json.NewEncoder(w).Encode(wallet.BitcoinWallet.Addresses)
+}
+
+// GetAddress returns address information for a specific address
+func GetAddress(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, ok := vars["id"]
+	if !ok {
+		http.Error(w, "No address ID specified", http.StatusBadRequest)
+		return
+	}
+
+	exists := wallet.CheckAddressExists(id)
+	if !exists {
+		http.Error(w, "Invalid address ID", http.StatusBadRequest)
+		return
+	}
+
+	// Fetch Address details
+	address := wallet.BitcoinWallet.GetAddress(id)
+	json.NewEncoder(w).Encode(address)
+}
+
+// AddAddress adds a new address to the wallet
+func AddAddress(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, ok := vars["id"]
+	if !ok {
+		http.Error(w, "No address ID specified", http.StatusBadRequest)
+		return
+	}
+
+	// Wallet must not have duplicate addresses
+	exists := wallet.CheckAddressExists(id)
+	if exists {
+		http.Error(w, "Address already exists", http.StatusConflict)
+		return
+	}
+
+	// Add new address to wallet
+	err := wallet.BitcoinWallet.AddAddress(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Println(w, "Address added with ID: %d", id)
+}
+
+// RemoveAddress archives an address from the wallet
+func RemoveAddress(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, ok := vars["id"]
+	if !ok {
+		http.Error(w, "No address ID specified", http.StatusBadRequest)
+		return
+	}
+
+	exists := wallet.CheckAddressExists(id)
+	if !exists {
+		http.Error(w, "Invalid address ID", http.StatusBadRequest)
+		return
+	}
+
+	// archive address
+	wallet.BitcoinWallet.RemoveAddress(id)
+	w.WriteHeader(http.StatusOK)
+}
